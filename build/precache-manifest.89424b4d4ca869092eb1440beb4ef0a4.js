b831c1838ddb37d2d5397fa526b5418bcbd8b5d4
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-11 08:05:11 -0300

    Adicionando códigos do app teste.

---
+++ build/precache-manifest.89424b4d4ca869092eb1440beb4ef0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240e55b9a991b41c7012d5fa96d65d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48d66cd7f6493b08440",
+    "url": "/static/css/main.7073c9df.chunk.css"
+  },
+  {
+    "revision": "5f4d2eb04cf622074043",
+    "url": "/static/js/2.09730f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09730f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48d66cd7f6493b08440",
+    "url": "/static/js/main.65a1422a.chunk.js"
+  },
+  {
+    "revision": "1655789032df59797759",
+    "url": "/static/js/runtime-main.b8451775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
