468421b7c3c4aff2883cc95f5ad937205a8f1694
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-11 08:12:02 -0300

    Updates

---
+++ precache-manifest.85783225b543e11e16e86375cc9c5b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c440ab757f92844e8956fa996aeebb62",
+    "url": "/pagesTeste/index.html"
+  },
+  {
+    "revision": "c726bdfeab527e5c37d9",
+    "url": "/pagesTeste/static/css/main.7073c9df.chunk.css"
+  },
+  {
+    "revision": "5f4d2eb04cf622074043",
+    "url": "/pagesTeste/static/js/2.09730f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pagesTeste/static/js/2.09730f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c726bdfeab527e5c37d9",
+    "url": "/pagesTeste/static/js/main.0d3bb3e4.chunk.js"
+  },
+  {
+    "revision": "21d5871120f39a000bb1",
+    "url": "/pagesTeste/static/js/runtime-main.c4e6fd3c.js"
+  }
+]);
